ff747d1ae6fccd0935a0ba9d8cc534772e223970
trushonlyyou <trushonlyyou@tnctec.co.kr>
2023-07-30 20:29:03 +0900

    Entity type mapping

--- src/main/java/me/whiteship/whiteship_jpa/Account.java
+++ src/main/java/me/whiteship/whiteship_jpa/Account.java
@@ -1,51 +1,29 @@
 package me.whiteship.whiteship_jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity // 자동으로 테이블로 메핑
+@Getter
+@Setter
 public class Account {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
 
-    private String email;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
 
-    public Long getId() {
-        return id;
-    }
+    private String yes;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    @Transient // 컬러으로 매핑 하지 않는다.
+    private String no;
 }
